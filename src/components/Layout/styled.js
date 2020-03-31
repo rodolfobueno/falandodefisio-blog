baf4ca9b8b41dc78aa93a9ac935c83e1db2dbac8
Rodolfo <rodolfo.bueno13@gmail.com>
2020-03-31 14:17:12 -0300

    correção do local onde foi inserido cor de fundo.

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -9,9 +9,9 @@ export const LayoutMain = styled.main`
   background: var(--background);
   min-height: 100vh;
   width: 100%;
+  background-color: #f3f3f3;
 `
 export const BodyWrapper = styled.div`
   padding-top: 6rem;
   padding-bottom: 4rem;
-  background-color: #f3f3f3;
 `
